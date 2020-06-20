d6d27127824822b466aa1b940b1b513f4a72e6da
Pranav S Kurup <pranavskurup@codedeck.dev>
2020-06-20 19:08:19 +0530

    Updates

---
+++ precache-manifest.620c83001cc502f5204bad56184c78a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ce1ce45f6be8b13db053bd1ec377fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e49f6a66d88757b78c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c72776ccfab2cf51fbdf",
+    "url": "/static/js/2.732b0290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.732b0290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e49f6a66d88757b78c9",
+    "url": "/static/js/main.2c82224b.chunk.js"
+  },
+  {
+    "revision": "e8e24b5b9a1fbfab61bc",
+    "url": "/static/js/runtime-main.a2fb3e62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
